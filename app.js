168545b29133770616b67d0838036526ad4071d7
muhammednasimda <muhammednasimda@gmail.com>
2021-01-29 18:12:00 +0530

    added routes

--- app.js
+++ app.js
@@ -8,4 +8,16 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.get("/test", (req, res) => {
+  res.send("working");
+});
+
+app.use("/payment", require("./routes/payment"));
+app.use("/cash-reciept", require("./routes/cash_reciept"));
+app.use("/customer", require("./routes/customer"));
+app.use("/exchange", require("./routes/exchange"));
+app.use("/exchange-cash-reciept", require("./routes/exchange_cash_reciept"));
+app.use("/purchase", require("./routes/purchase"));
+app.use("/sale", require("./routes/sale"));
+
 app.listen(PORT);
