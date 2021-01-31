276f0ef43ea04711d366f50f2c06563a9e3eca02
muhammednasimda <muhammednasimda@gmail.com>
2021-01-31 16:11:30 +0530

    added search feature

--- routes/customer.js
+++ routes/customer.js
@@ -3,11 +3,21 @@ const router = express.Router();
 const sequelize = require("../dbconnection");
 const initModels = require("../models/init-models");
 const models = initModels(sequelize);
+const Sequilize = require("sequelize");
+const Op = Sequilize.Op;
 
 //create a new customer
 router.post("/", async (req, res) => {
   const response = await models.customers.create({ ...req.body });
   res.json(response);
+});
+
+//search a customer
+router.get("/search/:searchTerm", async (req, res) => {
+  const searchResponse = await models.customers.findAll({
+    where: { customer_name: { [Op.like]: "%" + req.params.searchTerm + "%" } },
+  });
+  return res.json(searchResponse);
 });
 
 //get all customers
