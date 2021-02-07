3e5917ada266481ba264aca09902ed5d0f483407
hadi-a-azeez <hadikoduvally@gmail.com>
2021-02-07 23:27:39 +0530

    added get exchange of a user api

--- routes/exchange.js
+++ routes/exchange.js
@@ -4,16 +4,21 @@ const sequelize = require("../dbconnection");
 const initModels = require("../models/init-models");
 const models = initModels(sequelize);
 
-router.get("/:customer_id", async (req, res) => {
-  const response = await models.exchange.findAll({
-    where: { customer_id: req.params.customer_id },
-  });
-  return res.json(response);
-});
-
 router.post("/", async (req, res) => {
   const response = await models.exchange.create({ ...req.body });
   return res.json(response);
+});
+
+//get all exchanges of a user
+router.get("/:customer_id", async (req, res) => {
+  const sum_of_exchanges = await models.exchange.sum("currency_total", {
+    where: { customer_id: req.params.customer_id },
+  });
+
+  const exchanges = await models.exchange.findAll({
+    where: { customer_id: req.params.customer_id },
+  });
+  res.json({ sum_of_exchanges, exchanges });
 });
 
 module.exports = router;
